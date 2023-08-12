76caea7006fc76c63d5c048b21779e92ea5a12cb
Allie Fehr <fehr.35@osu.edu>
2023-08-12 12:05:59 -0400

    Initial Commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by Allie Fehr on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
